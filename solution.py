cee8c6fdbd64f609496119e7e906adb0df6ee667
Jose Velazquez <jav4534@nyu.edu>
2022-01-27 00:13:14 -0500

    Fixed answers for questions 5 and 7

--- solution.py
+++ solution.py
@@ -20,11 +20,11 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         message = 'NYU Computer Networking'
-        answer = hashlib.md5(message)
+        answer = hashlib.md5(message.encode())
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 7
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
     return(answer)
