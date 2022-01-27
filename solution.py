1b1762d258534190db89243771799b7df06eb314
Jose Velazquez <jav4534@nyu.edu>
2022-01-27 00:09:35 -0500

    First submission to solve lab. Added all the questions and tentative answers

--- solution.py
+++ solution.py
@@ -1,14 +1,32 @@
-### welcome_assignment_answers
+    ### welcome_assignment_answers
 ### Input - All eight questions given in the assignment.
 ### Output - The right answer for the specific question.
+
+import hashlib
 
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    
+    answer = ""
+    
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "mtls"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        message = 'NYU Computer Networking'
+        answer = hashlib.md5(message)
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer = 7
+    elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = 4
     return(answer)
 # Complete all the questions.
 
